25d8a0e6c6cee8159b52d92ebd8911b82ea4f63e
sechs <philippe.buteau@skema.edu>
2021-09-10 12:24:06 +0200

    commit test after debugging git

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
         <div className="menu-item">
           <div className="content">
             <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP</span>
+            <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
         <div className="menu-item">
