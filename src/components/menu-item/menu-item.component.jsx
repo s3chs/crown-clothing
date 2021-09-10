92f613858f79ad2d90e713a6f1006db4afd14d9f
sechs <philippe.buteau@skema.edu>
2021-09-10 16:40:28 +0200

    dynamic routing done

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router";
 
-export default function MenuItem({ title, imageUrl, size }) {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -15,4 +19,6 @@ export default function MenuItem({ title, imageUrl, size }) {
       </div>
     </div>
   );
-}
+};
+
+export default withRouter(MenuItem);
